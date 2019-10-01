dce7899bba06db6655a1b7a5349eea86f36b95b8
Marc <marc@bekhuis.net>
2019-10-01 16:00:47 +0200

    well then

--- Zombie Shooter/Assets/Scripts/New Version/MapGenerator.cs
+++ Zombie Shooter/Assets/Scripts/New Version/MapGenerator.cs
@@ -10,24 +10,51 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        List<GameObject> islandParts = new List<GameObject>();
-        for (int x = 0; x < 10; x++)
+        Vector2Int islandSize = new Vector2Int(Random.Range(3,13), Random.Range(3, 13));
+        GameObject[,] islandParts = new GameObject[islandSize.x,islandSize.y];
+        for (int x = 0; x < islandSize.x ; x++)
         {
-            if (islandParts.Count == 0)
+            for (int y = 0; y < islandSize.y; y++)
             {
-                Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
-                Vector3 location = new Vector3(Random.Range(0, 2), 0, Random.Range(0, 2));
-                islandParts.Add(Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
-                islandParts[islandParts.Count - 1].transform.localScale = size;
-            }
-            else
-            {
-                Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
-                Vector3 location = new Vector3(islandParts[islandParts.Count - 1].transform.position.x + size.x / 2 + islandParts[islandParts.Count - 1].transform.localScale.x /2, 0, islandParts[islandParts.Count - 1].transform.position.z);
-                islandParts.Add(Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
-                islandParts[islandParts.Count - 1].transform.localScale = size;
+                if (x == 0 && y == 0)
+                {
+                    Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+                    Vector3 location = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+                    islandParts[x, y] = (Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+                    islandParts[x, y].transform.localScale = size;
+                }
+                else if (x != 0 && y == 0)
+                {
+                    Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+                    Vector3 location = new Vector3(islandParts[x - 1, y].transform.position.x + size.x / 2 + islandParts[x - 1, y].transform.localScale.x / 2, 0, this.transform.position.z);
+                    islandParts[x, y] = (Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+                    islandParts[x, y].transform.localScale = size;
+                }
+                else if (x == 0 && y != 0)
+                {
+                    Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+                    Vector3 location = new Vector3(this.transform.position.x, 0, islandParts[x, y - 1].transform.position.z + size.z / 2 + islandParts[x, y - 1].transform.localScale.z / 2);
+                    islandParts[x, y] = (Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+                    islandParts[x, y].transform.localScale = size;
+                }
             }
         }
+
+        //List<GameObject> islandParts = new List<GameObject>();
+        //if (islandParts.Length == 0)
+        //{
+        //    Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+        //    Vector3 location = new Vector3(Random.Range(0, 2), 0, Random.Range(0, 2));
+        //    islandParts[2, 2] = (Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+        //    islandParts[2, 2].transform.localScale = size;
+        //}
+        //else
+        //{
+        //    Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+        //    Vector3 location = new Vector3(islandParts[islandParts.Count - 1].transform.position.x + size.x / 2 + islandParts[islandParts.Count - 1].transform.localScale.x / 2, 0, islandParts[islandParts.Count - 1].transform.position.z);
+        //    islandParts.Add(Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+        //    islandParts[islandParts.Count - 1].transform.localScale = size;
+        //}
     }
 
     // Update is called once per frame
