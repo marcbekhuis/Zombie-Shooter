4aa9ba95d7767e85a5a0e3de0d06d9ca8c22555c
Marc <marc@bekhuis.net>
2019-10-01 11:54:38 +0200

    lol

---
+++ Zombie Shooter/Assets/Scripts/New Version/MapGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject grass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<GameObject> islandParts = new List<GameObject>();
+        for (int x = 0; x < 10; x++)
+        {
+            if (islandParts.Count == 0)
+            {
+                Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+                Vector3 location = new Vector3(Random.Range(0, 2), 0, Random.Range(0, 2));
+                islandParts.Add(Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+                islandParts[islandParts.Count - 1].transform.localScale = size;
+            }
+            else
+            {
+                Vector3 size = new Vector3(Random.Range(4, 8) * 5, 1, Random.Range(4, 8) * 5);
+                Vector3 location = new Vector3(islandParts[islandParts.Count - 1].transform.position.x + size.x / 2 + islandParts[islandParts.Count - 1].transform.localScale.x /2, 0, islandParts[islandParts.Count - 1].transform.position.z);
+                islandParts.Add(Instantiate(grass, location, new Quaternion(0, 0, 0, 0), this.transform));
+                islandParts[islandParts.Count - 1].transform.localScale = size;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
